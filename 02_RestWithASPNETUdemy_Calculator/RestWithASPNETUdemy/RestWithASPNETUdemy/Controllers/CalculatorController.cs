97e9ee403486b7f4cbd6205bb1c311cb6b476539
Bernardo L S P Fusco de S Guerra <bernardo_fusco@hotmail.com>
2024-03-14 14:32:43 -0300

    Adicionando demais funções

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -14,17 +14,90 @@ public class CalculatorController : ControllerBase
 		_logger = logger;
 	}
 
-	[HttpGet ("sum/{firstNumber}/{secondNumber}")]
-	public IActionResult Get(string firstNumber, string secondNumber)
+	// SOMA
+	[HttpGet ("soma/{firstNumber}/{secondNumber}")]
+	public IActionResult Soma(string firstNumber, string secondNumber)
 	{
 		if ( isNumeric(firstNumber) && isNumeric(secondNumber) ) { 
 
 			var sum = Convert.ToDecimal (firstNumber) + Convert.ToDecimal(secondNumber);
 
-			return Ok("Resultado = " + sum.ToString());
+			return Ok("Soma = " + sum.ToString());
 		}
 		return BadRequest("Número Inválido!");
 	}
+
+	// SUBTRAÇÃO
+	[HttpGet("sub/{firstNumber}/{secondNumber}")]
+	public IActionResult Subtracao(string firstNumber, string secondNumber)
+	{
+		if (isNumeric(firstNumber) && isNumeric(secondNumber))
+		{
+
+			var sum = Convert.ToDecimal(firstNumber) - Convert.ToDecimal(secondNumber);
+
+			return Ok("Subtração = " + sum.ToString());
+		}
+		return BadRequest("Número Inválido!");
+	}
+
+	// MULTIPLICAÇÃO
+	[HttpGet("mult/{firstNumber}/{secondNumber}")]
+	public IActionResult Multiplicacao(string firstNumber, string secondNumber)
+	{
+		if (isNumeric(firstNumber) && isNumeric(secondNumber))
+		{
+
+			var sum = Convert.ToDecimal(firstNumber) * Convert.ToDecimal(secondNumber);
+
+			return Ok("Multiplicação = " + sum.ToString());
+		}
+		return BadRequest("Número Inválido!");
+	}
+
+	// DIVISÃO
+	[HttpGet("div/{firstNumber}/{secondNumber}")]
+	public IActionResult Divisao(string firstNumber, string secondNumber)
+	{
+		if (isNumeric(firstNumber) && isNumeric(secondNumber))
+		{
+
+			var sum = Convert.ToDecimal(firstNumber) / Convert.ToDecimal(secondNumber);
+
+			return Ok("Divisão = " + sum.ToString());
+		}
+		return BadRequest("Número Inválido!");
+	}
+
+	// MÉDIA
+	[HttpGet("media/{firstNumber}/{secondNumber}")]
+	public IActionResult Media(string firstNumber, string secondNumber)
+	{
+		if (isNumeric(firstNumber) && isNumeric(secondNumber))
+		{
+
+			var sum = ( Convert.ToDecimal(firstNumber) + Convert.ToDecimal(secondNumber) ) / 2 ;
+
+			return Ok("Média = " + sum.ToString());
+		}
+		return BadRequest("Número Inválido!");
+	}
+
+	// RAIZ QUADRADA
+	[HttpGet("raiz/{firstNumber}/{secondNumber}")]
+	public IActionResult RaizQuadrada(string firstNumber, string secondNumber)
+	{
+		if (isNumeric(firstNumber) && isNumeric(secondNumber))
+		{
+			var sum = Convert.ToDecimal(firstNumber) + Convert.ToDecimal(secondNumber);
+
+			var raiz = Math.Sqrt( Convert.ToDouble(sum));
+
+			return Ok("Raiz Quadrada = " + raiz.ToString());
+		}
+		return BadRequest("Número Inválido!");
+	}
+
 
 	private bool isNumeric(string strNumber)
 	{
